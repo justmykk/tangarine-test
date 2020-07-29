cd337e358cafccd66f48d30e4889e3e2e5bbb015
Michael Olawale <olise.c.okpala@firstbanknigeria.com>
2020-07-29 15:08:56 +0100

    drag and drop done

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -2,17 +2,11 @@ import React, { useEffect } from 'react';
 import cloud from '../cloud.svg';
 import readXlsxFile from 'read-excel-file';
 
-export const Upload = ({onUploadDone}) => {
+export const Upload = ({ onUploadDone }) => {
     useEffect(() => {
         let el = document.getElementById('file');
 
-        const handleFileInput = () => {
-            readXlsxFile(el.files[0])
-                .then((ev) => {
-                    onUploadDone({data: ev, file: el.files[0]});
-                })
-                .catch((error) => console.log('xls error', error));
-        };
+        const handleFileInput = () => {readFile(el.files[0])};
 
         el.addEventListener('change', handleFileInput);
 
@@ -24,9 +18,28 @@ export const Upload = ({onUploadDone}) => {
         el.click();
     };
 
+    const onDrop = (ev) => {
+        ev.preventDefault();
+        readFile(ev.dataTransfer.files[0]);
+    };
+
+    const readFile = (file) => {
+        readXlsxFile(file)
+            .then((ev) => {
+                onUploadDone({ data: ev, file });
+            })
+            .catch((error) => console.log('xls error', error));
+    };
+
     return (
         <>
-            <div className="upload">
+            <div
+                className="upload"
+                onDragEnter={(ev) => ev.preventDefault()}
+                onDragLeave={(ev) => ev.preventDefault()}
+                onDragOver={(ev) => ev.preventDefault()}
+                onDrop={onDrop}
+            >
                 <img src={cloud} alt="upload" style={{ height: 120 }} />
                 <p>Drag and drop a file here</p>
                 <p>or</p>
